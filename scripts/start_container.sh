010955a60c726aa8b9ef81f8ed45dba3b336c6dd
georgeonalo <115881685+georgeonalo@users.noreply.github.com>
2024-01-22 21:53:25 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker docker pull georgenal/simple-python-flask-app
+docker pull georgenal/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 georgenal/simple-python-flask-app
